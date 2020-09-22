9505ca7adba2bc2dae62360327d81e5dd78a71d4
Thomas De Lora <TED@Phileas.local>
2020-09-22 13:42:26 -0700

    Beginning work on awsTerminate

--- aws_icc.py
+++ aws_icc.py
@@ -1,4 +1,28 @@
 import sys,boto3
+from botocore.exceptions import ClientError
+
+def aws_instance_state(instanceIdReq):
+        ec2 = boto3.client('ec2')
+
+        try:
+                response = ec2.describe_instances()
+        except ClientError as e:
+	        # Somthing went wrong with the describe instances query
+		# print(e.response)
+                print("aws_icc.aws_instance_state: Unexpected error: %s" % e)
+        else:
+                instanceList = response.get('Instances')
+                if instanceList:
+                        # Somthing is running
+                        for instanceDictionary in instanceList:
+                                instanceId = instanceDictionary.get("InstanceId")
+                                if instanceId != None:
+                                        if instanceId == instanceIdReq:
+                                                print("Instance: %s" % instanceDictionary)
+                                                break
+                                                
+
+
 
 def aws_create_instance(iid,it,kn):
 	ec2_resource = boto3.resource('ec2')
