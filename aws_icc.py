43a473fe67afb37fb5f33884b31b495e027893c7
Thomas De Lora <TED@Phileas.local>
2020-09-28 22:12:11 -0700

    Safety chek in, almost there

--- aws_icc.py
+++ aws_icc.py
@@ -23,11 +23,8 @@ def aws_instance_state(instanceIdReq):
                                         stateCode = state.get('Code')
                                         stateString = state.get('Name')
                                         # print(instanceId,stateString)
-        return(stateCode,stateString)            
+        return(stateCode,stateString)
                                                 
-
-
-
 def aws_create_instance(iid,it,kn):
 	ec2_resource = boto3.resource('ec2')
 
@@ -40,9 +37,33 @@ def aws_create_instance(iid,it,kn):
 
 	return instance
 
-def aws_terminate_instance(instanceId):
+def aws_get_instances_with_state(reqState):
+        ec2 = boto3.client('ec2')
+        stateCode = -1
+        inList = []
+        print(reqState)
+
+        try:
+                response = ec2.describe_instances()
+        except ClientError as e:
+                # Somthing went wrong with the describe instances query
+	        # print(e.response)
+                print("aws_icc.aws_get_instances_with_state: Unexpected error: %s" % e)
+        else:
+                reservationsList = response.get('Reservations')
+                for listData in reservationsList:
+                        instanceInfo = listData.get("Instances")
+                        for info in instanceInfo:
+                                state = info['State']
+                                stateCode = state.get('Code')
+                                if stateCode == reqState:
+                                        instanceId = info['InstanceId']
+                                        # print(instanceId,stateCode)
+                                        inList.append(instanceId)
+        return inList
+
+def aws_terminate_instance(instanceList):
         ec2_resource = boto3.resource('ec2')
-        instanceList= [instanceId]
 
         try:
                 ec2_resource.instances.filter(InstanceIds=instanceList).terminate()
