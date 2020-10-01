410c35213bde605ad80e55a321fdae3cee143291
Thomas De Lora <TED@Phileas.local>
2020-09-30 19:44:20 -0700

    Refactored aws_icc.py to eliminanate code redundancy + removed extra print from awsCreate

--- aws_icc.py
+++ aws_icc.py
@@ -1,28 +1,47 @@
 import sys,boto3
 from botocore.exceptions import ClientError
 
-def aws_instance_state(instanceIdReq):
+def get_instances_and_state():
         ec2 = boto3.client('ec2')
-        stateCode = -1
-        stateString = "notFound"
+        isdList = []
 
         try:
                 response = ec2.describe_instances()
         except ClientError as e:
 	        # Somthing went wrong with the describe instances query
 		# print(e.response)
-                print("aws_icc.aws_instance_state: Unexpected error: %s" % e)
+                print("aws_icc.get_instances_and_state: Unexpected error: %s" % e)
         else:
                 reservationsList = response.get('Reservations')
                 for listData in reservationsList:
                         instanceInfo = listData.get("Instances")
                         for info in instanceInfo:
                                 instanceId = info['InstanceId']
-                                if instanceId == instanceIdReq:
-                                        state = info['State']
-                                        stateCode = state.get('Code')
-                                        stateString = state.get('Name')
-                                        # print(instanceId,stateString)
+                                state = info['State']
+                                stateCode = state.get('Code')
+                                stateString = state.get('Name')
+                                # print(instanceId,stateCode.stateString)
+                                isDict = {'InstanceId':instanceId,'Code':stateCode,'Name':stateString}
+                                isdList.append(isDict)
+        return isdList
+
+def aws_instance_state(instanceIdReq):
+        ec2 = boto3.client('ec2')
+        stateCode = -1
+        stateString = "notFound"
+
+        isdList = get_instances_and_state()
+
+        for isd in isdList:
+                instanceId = isd.get('InstanceId')
+                if instanceId == None:
+                        print("aws_icc.aws_instance_state: InstanceId not found!")
+                else:
+                        if instanceId == instanceIdReq:
+                                stateCode = isd.get('Code')
+                                stateString = isd.get('Name')
+                                break
+                
         return(stateCode,stateString)
                                                 
 def aws_create_instance(iid,it,minC,maxC,kn):
@@ -43,23 +62,14 @@ def aws_get_instances_with_state(reqState):
         inList = []
         # print(reqState)
 
-        try:
-                response = ec2.describe_instances()
-        except ClientError as e:
-                # Somthing went wrong with the describe instances query
-	        # print(e.response)
-                print("aws_icc.aws_get_instances_with_state: Unexpected error: %s" % e)
-        else:
-                reservationsList = response.get('Reservations')
-                for listData in reservationsList:
-                        instanceInfo = listData.get("Instances")
-                        for info in instanceInfo:
-                                state = info['State']
-                                stateCode = state.get('Code')
-                                if stateCode == reqState:
-                                        instanceId = info['InstanceId']
-                                        # print(instanceId,stateCode)
-                                        inList.append(instanceId)
+        isdList = get_instances_and_state()
+
+        for isd in isdList:
+                stateCode = isd.get('Code')
+                if stateCode == reqState:
+                        instanceId = isd.get('InstanceId')
+                        inList.append(instanceId)
+        
         return inList
 
 def aws_terminate_instance(instanceList):
