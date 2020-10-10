d452070dd069b9658be3d466ef24556801b202e5
Thomas De Lora <TED@Phileas.local>
2020-10-10 13:08:39 -0700

    Safety check in - aws_lambda.py appears to be ready, awsCreate modified to call Lambda functionality. Lambda side still pending.

--- aws_lambda.py
+++ aws_lambda.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 def trigger_event(function,payload):
-
+    triggerSuccess = False
     client = boto3.client('lambda')
     try:
         response = client.invoke(
@@ -15,6 +15,24 @@ def trigger_event(function,payload):
                 # Somthing went wrong with the invokation
                 print("aws_lamda.trigger_event: %s" % e)
     else:
-        print(response)
+        # response is a dictionary
+        metaData = response.get('ResponseMetadata')
+        if metaData == None:
+            # This should never happen unless they changed the response
+            raise KeyError("aws_lambda.trigger_event: ResponseMetadata not found!")
+        else:
+            # metaData contains key pair HTTPStatusCode, we are checking for code 202
+            # which means the event was accepted but not acted on yet.
+            HTTPStatusCode = metaData.get('HTTPStatusCode')
+            if HTTPStatusCode == None:
+                # This should never happen unless they changed the response
+                raise KeyError("aws_lambda.trigger_event: HTTPStatusCode not found!")
+            else:
+                if HTTPStatusCode == 202:
+                    # All went well
+                    triggerSuccess = True
+                else:
+                    # Again with the issues!
+                    print("aws_lambda.trigger_event: received unexpected response code {}!".format(HTTPStatusCode))
 
-    # return(response)
+    return(triggerSuccess)
