062f841e01d46e4ec322dc84cc8dc3d63c9a396a
Thomas De Lora <TED@Phileas.local>
2020-11-05 21:27:55 -0800

    Safety checkin

--- aws_event.py
+++ aws_event.py
@@ -7,7 +7,7 @@ def put_lambda_event(ruleName,scheduleExpression,arn,id):
     response = aws_put_rule(ruleName,scheduleExpression)
 
     if response:
-        response = aws_put_target(ruleName,arn,id)
+        response = aws_put_targets(ruleName,arn,id)
         if response:
             returnCode = True
     
@@ -24,8 +24,12 @@ def aws_put_rule(ruleName,scheduleExpression):
 
     response = client.put_rule(Name=ruleName,ScheduleExpression=scheduleExpression,State='ENABLED')
 
-    print(response)
-    returnCode = True
+    httpStatusCode = getHttpsStatusCode(response)
+    if httpStatusCode == 200:
+            # All went well
+            returnCode = True
+    else:
+            print("aws_event.aws_put_rule: received unexpected http status code {}" % httpStatusCode)
 
     return returnCode
 
@@ -44,13 +48,35 @@ def aws_delete_rule(ruleName):
     
     pass
 
-def aws_put_target(ruleName,arn,id):
+def aws_put_targets(ruleName,arn,id):
     client = boto3.client('events')
     returnCode = False
-    
+
+    targetsList = [{"Arn":"arn:aws:lambda:us-west-1:985167159610:function:term-running-ec2-instances","Id":"term-running-ec2-instances"}]
+    response = client.put_targets(Rule=ruleName,Targets=targetsList)
+
+    httpStatusCode = getHttpsStatusCode(response)
+    if httpStatusCode == 200:
+            # All went well
+            returnCode = True
+    else:
+            print("aws_event.aws_put_targets: received unexpected http status code {}" % httpStatusCode)
+
     return returnCode
 
 def aws_remove_target(ruleName,id):
     client = boto3.client('events')
     
     pass
+
+def getHttpsStatusCode(response):
+    returnStatusCode = 404
+    responseMetadata = response.get('ResponseMetadata')
+    if responseMetadata:
+        httpStatusCode = responseMetadata.get('HTTPStatusCode')
+        if httpStatusCode:
+            returnStatusCode = httpStatusCode
+    else:
+        print("aws_event.httpsStatusCode: Expected key ResponseMetadata not found")
+
+    return returnStatusCode
